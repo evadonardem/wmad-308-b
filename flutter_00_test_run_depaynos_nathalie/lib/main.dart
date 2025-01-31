ecfc9dc75b72122062667aa56f40fd5d03b3d0f0
oknfhd <anfhdc@gmail.com>
2025-01-31 18:54:34 +0800

    Flutter Test Run

---
+++ flutter_00_test_run_depaynos_nathalie/lib/main.dart
@@ -0,0 +1,86 @@
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Test Run by Nathalie Depaynos',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 195, 0, 255)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Test Run Home Page by Nathalie Depaynos'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _counterDirection(bool isIncrement){
+    setState(() {
+      _counter += isIncrement ? 1 : -1; 
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   
+    return Scaffold(
+      appBar: AppBar(
+       
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+       
+        child: Column(
+      
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => _counterDirection(false), 
+                    child: const Icon(Icons.yard_sharp),
+                    ),
+                    const SizedBox(width: 10,),
+                  ElevatedButton(
+                    onPressed: () => _counterDirection(true), 
+                    child: const Icon(Icons.yard_outlined),
+                    ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
