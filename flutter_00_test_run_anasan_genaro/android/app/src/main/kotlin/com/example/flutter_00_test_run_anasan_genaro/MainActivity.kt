ae2a1159a966031893d4c37c8fa105323bdd81ae
DinosaurToothbrush <2023-01-0249@kcp.edu.ph>
2025-02-01 09:25:56 +0800

    asd

---
+++ flutter_00_test_run_anasan_genaro/android/app/src/main/kotlin/com/example/flutter_00_test_run_anasan_genaro/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_00_test_run_anasan_genaro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ flutter_00_test_run_anasan_genaro/lib/main.dart
@@ -0,0 +1,90 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Test Run By:Anasan Genaro',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 1, 12, 3)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Test Run Homepage By: Anasan Genaro'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  _counterDirection(bool isIncrement){
+    setState(() {
+      _counter += isIncrement ? 1: -1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   
+    return Scaffold(
+      appBar: AppBar(
+     
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+       
+        title: Text(widget.title),
+      ),
+      body: Center(
+       
+        child: Column(
+         
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () => _counterDirection(false),
+                  child: const Icon(Icons.monochrome_photos),
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () => _counterDirection(true),
+                  child: const Icon(Icons.sunny_snowing),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'My Name is Genaro',
+              style: TextStyle(fontSize: 16, color: Color.fromARGB(255, 0, 70, 223)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
